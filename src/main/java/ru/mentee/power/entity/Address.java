633933c04f20bcc473d238725a762f400190e559
pavelKomiss121 <pavelkomiss@gmail.com>
2025-12-13 22:22:51 +0300

    178 - создание embeddable объектов

---
+++ src/main/java/ru/mentee/power/entity/Address.java
@@ -0,0 +1,32 @@
+package ru.mentee.power.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Встраиваемый объект адреса с переопределением колонок.
+ */
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    @Column(name = "street", length = 255)
+    private String street;
+
+    @Column(name = "city", length = 100)
+    private String city;
+
+    @Column(name = "state", length = 50)
+    private String state;
+
+    @Column(name = "zip_code", length = 20)
+    private String zipCode;
+
+    @Column(name = "country", length = 2)
+    private String countryCode;
+}
